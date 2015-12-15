40e074988abf7969a9f80b75fa164542d0a3c2ab
Isabell Kiral-Kornek <isa.kiral@gmx.de>
2015-12-16 10:52:29 +1100

    wow, i don't commit very often

--- main.js
+++ main.js
@@ -19,6 +19,11 @@ var speech_w = 140;
 var speech_h = 80;
 
 
+var twi_viz = d3.select("#twit_resplat").style("display","none")
+.style("position","absolute")
+.style("top","350px")
+.style("left","10px");
+
 d3.selection.prototype.moveToFront = function(){
 	return this.each(function(){
 		this.parentNode.appendChild(this);
@@ -76,7 +81,128 @@ var viz = d3.select("#viz").attr("x",380).attr("y",310).attr("class","platy");
 viz.moveToFront();
 
 
+//// PAGES //// 
 
+
+// WRANGLER
+speech_wra = d3.select("body").append("div")
+		.style("background-color","white").style("position","absolute")
+		.style("top","40px").style("left","270px")
+		.style("padding","10px")
+		.text("I am rounding up and analysing data!").moveToFront()
+		.style("border-radius","5px")
+		.style("opacity",0)
+		.style("width","100px")
+		.style("font-family","sans-serif")
+		.attr("class","hidden");
+
+page_wra = d3.select("body").append("div").attr("class","wra_div")
+				.style("width",mommy_w+"px")
+				.style("opacity",0)
+				.style("position","absolute")
+				.style("top","10px")
+				.style("left","10px")
+				.style("visibility","hidden");
+
+page_wra.append("div").text("Data Wrangling!").style("font-family","sans-serif")
+		.style("font-size","2em").style("color","white")
+		.style("padding","20px");
+
+page_wra.append("div").text("blurb")
+		.style("font-family","sans-serif")
+		.style("font-size","1em").style("color","white")
+		.style("padding","20px");
+
+page_wra.append("img").attr("src","kerry.jpg")
+		.style("width","80px")
+		.style("border-radius","100px")
+		.style("padding","20px");
+
+page_wra.append("img").attr("src","pippa.jpg")
+		.style("width","80px")
+		.style("border-radius","100px")
+		.style("padding","20px");
+
+// CADVENTURER
+speech_cad = d3.select("body").append("div")
+		.style("background-color","white").style("position","absolute")
+		.style("top","30px").style("left","120px")
+		.style("padding","10px")
+		.text("I am playing and printing in 3D!").moveToFront()
+		.style("border-radius","5px")
+		.style("opacity",0)
+		.style("width","100px")
+		.style("font-family","sans-serif")
+		.attr("class","hidden");
+
+page_cad = d3.select("body").append("div").attr("class","cad_div")
+				.style("width",mommy_w+"px")
+				.style("opacity",0)
+				.style("position","absolute")
+				.style("top","10px")
+				.style("left","10px")
+				.style("visibility","hidden");
+
+page_cad.append("div").text("Come on a CADventure!").style("font-family","sans-serif")
+		.style("font-size","2em").style("color","white")
+		.style("padding","20px");
+
+page_cad.append("div").text("blurb.")
+		.style("font-family","sans-serif")
+		.style("font-size","1em").style("color","white")
+		.style("padding","20px");
+
+page_cad.append("img").attr("src","paul.jpg")
+		.style("width","80px")
+		.style("border-radius","100px")
+		.style("padding","20px");
+
+page_cad.append("img").attr("src","louise.jpg")
+		.style("width","80px")
+		.style("border-radius","100px")
+		.style("padding","20px");
+
+// DAVINCI
+speech_dav = d3.select("body").append("div")
+		.style("background-color","white").style("position","absolute")
+		.style("top","330px").style("left","130px")
+		.style("padding","10px")
+		.text("I am mining and analysing social data!").moveToFront()
+		.style("border-radius","5px")
+		.style("opacity",0)
+		.style("width","100px")
+		.style("font-family","sans-serif")
+		.attr("class","hidden");
+
+page_dav = d3.select("body").append("div").attr("class","dav_div")
+				.style("width",mommy_w+"px")
+				.style("opacity",0)
+				.style("position","absolute")
+				.style("top","10px")
+				.style("left","10px")
+				.style("visibility","hidden");
+
+page_dav.append("div").text("Text mining!").style("font-family","sans-serif")
+		.style("font-size","2em").style("color","white")
+		.style("padding","20px");
+
+page_dav.append("div").text("blurb.")
+		.style("font-family","sans-serif")
+		.style("font-size","1em").style("color","white")
+		.style("padding","20px");
+
+page_dav.append("img").attr("src","kim.jpg")
+		.style("width","80px")
+		.style("border-radius","100px")
+		.style("padding","20px");
+
+page_dav.append("img").attr("src","yuandra.jpg")
+		.style("width","80px")
+		.style("border-radius","100px")
+		.style("padding","20px");
+
+
+// VIZARD
 speech_viz = d3.select("body").append("div")
 		.style("background-color","white").style("position","absolute")
 		.style("top","340px").style("left","280px")
@@ -115,65 +241,174 @@ page_viz.append("img").attr("src","errol.jpg")
 		.style("padding","20px");
 
 
+
 // rad circle animation
 var cad_state = 1;
 var wra_state = 1;
 var dav_state = 1;
 var viz_state = 1;
 
-circ_cad.on("click", function(){
-	if (cad_state == 1){
-		d3.selectAll(".element").transition().duration(trans_op).attr("opacity",0);
-		d3.selectAll(".op_element").transition().duration(trans_op).attr("opacity",0);
-		d3.selectAll(".op7_element").transition().duration(trans_op).attr("opacity",0);
-		d3.select(this).transition().duration(trans_exp).attr("r",expand_rad).attr("opacity",1);
-		cad_state = 0;
-	}
-	else {
-		d3.selectAll(".element").transition().delay(trans_exp-2*trans_op).duration(trans_op).attr("opacity",opacity);
-		d3.selectAll(".op_element").transition().delay(trans_op).duration(trans_op).attr("opacity",1);
-		d3.selectAll(".op7_element").transition().delay(trans_op).duration(trans_op).attr("opacity",.7);
-		d3.select(this).transition().duration(trans_exp).attr("r",rad).attr("opacity",opacity);
-		cad_state = 1;
-	
-	}
-});
+//// PAGE INTERACTION
 
-circ_wra.on("click", function(){
+// WRANGLER
+
+wra.on("click", function(){
 	if (wra_state == 1){
 		d3.selectAll(".element").transition().duration(trans_op).attr("opacity",0);
 		d3.selectAll(".op_element").transition().duration(trans_op).attr("opacity",0);
 		d3.selectAll(".op7_element").transition().duration(trans_op).attr("opacity",0);
-		d3.select(this).transition().duration(trans_exp).attr("r",expand_rad).attr("opacity",1);
+		d3.selectAll(".platy").transition().duration(trans_op).attr("opacity",0);
+		circ_wra.transition().duration(trans_exp).attr("r",expand_rad).attr("opacity",1);
+		d3.select(this).transition().attr("opacity",1);
+		speech_wra.transition().duration(trans_sp).style("opacity",0);
+
+		page_wra.style("visibility","visible");
+		page_wra.transition().duration(trans_exp).style("opacity",1);
+
+		wra.transition("ease").delay(200).duration(trans_exp).attr("x",335).attr("y",310);
+
 		wra_state = 0;
+		d3.select(this).moveToFront();
+
+		dav.moveToBack();
+		viz.moveToBack();
+		cad.moveToBack();
+
 	}
 	else {
 		d3.selectAll(".element").transition().delay(trans_exp-2*trans_op).duration(trans_op).attr("opacity",opacity);
 		d3.selectAll(".op_element").transition().delay(trans_op).duration(trans_op).attr("opacity",1);
+		d3.selectAll(".platy").transition().duration(trans_exp-2).attr("opacity",1);
 		d3.selectAll(".op7_element").transition().delay(trans_op).duration(trans_op).attr("opacity",.7);
-		d3.select(this).transition().duration(trans_exp).attr("r",rad).attr("opacity",opacity);
+		circ_wra.transition().duration(trans_exp).attr("r",rad).attr("opacity",opacity);
+
+		page_wra.transition().duration(trans_exp).style("opacity",0);
+		page_wra.style("visibility","hidden");
+
+		wra.transition("ease").duration(trans_exp).attr("x",335).attr("y",45);
+
 		wra_state = 1;
+		
+		dav.moveToFront();
+		viz.moveToFront();
+		cad.moveToFront();
 	
 	}
 });
 
-circ_dav.on("click", function(){
-	if (dav_state == 1){
+wra.on("mouseover", function(){
+	if (wra_state == 1){
+	speech_wra.transition().duration(trans_sp).style("opacity",0.7);}
+})
+wra.on("mouseout", function(){
+	speech_wra.transition().duration(trans_sp).style("opacity",0);
+})
+
+// CADVENTURER
+
+cad.on("click", function(){
+	if (cad_state == 1){
 		d3.selectAll(".element").transition().duration(trans_op).attr("opacity",0);
 		d3.selectAll(".op_element").transition().duration(trans_op).attr("opacity",0);
 		d3.selectAll(".op7_element").transition().duration(trans_op).attr("opacity",0);
-		d3.select(this).transition().duration(trans_exp).attr("r",expand_rad).attr("opacity",1);
-		dav_state = 0;
+		d3.selectAll(".platy").transition().duration(trans_op).attr("opacity",0);
+		circ_cad.transition().duration(trans_exp).attr("r",expand_rad).attr("opacity",1);
+		d3.select(this).transition().attr("opacity",1);
+		speech_cad.transition().duration(trans_sp).style("opacity",0);
+
+		page_cad.style("visibility","visible");
+		page_cad.transition().duration(trans_exp).style("opacity",1);
+		
+
+		cad.transition("ease").delay(200).duration(trans_exp).attr("x",10).attr("y",310);
+
+		cad_state = 0;
+		d3.select(this).moveToFront();
+		dav.moveToBack();
+		viz.moveToBack();
+		wra.moveToBack();
 	}
 	else {
 		d3.selectAll(".element").transition().delay(trans_exp-2*trans_op).duration(trans_op).attr("opacity",opacity);
 		d3.selectAll(".op_element").transition().delay(trans_op).duration(trans_op).attr("opacity",1);
+		d3.selectAll(".platy").transition().duration(trans_exp-2).attr("opacity",1);
 		d3.selectAll(".op7_element").transition().delay(trans_op).duration(trans_op).attr("opacity",.7);
-		d3.select(this).transition().duration(trans_exp).attr("r",rad).attr("opacity",opacity);
-		dav_state = 1;
+		circ_cad.transition().duration(trans_exp).attr("r",rad).attr("opacity",opacity);
+
+		page_cad.transition().duration(trans_exp).style("opacity",0);
+		page_cad.style("visibility","hidden");
+
+		cad.transition("ease").duration(trans_exp).attr("x",10).attr("y",35);
+
+		cad_state = 1;
+		dav.moveToFront();
+		viz.moveToFront();
+		wra.moveToFront();
 	
 	}
 });
+
+cad.on("mouseover", function(){
+	if (cad_state == 1){
+	speech_cad.transition().duration(trans_sp).style("opacity",0.7);}
+})
+cad.on("mouseout", function(){
+	speech_cad.transition().duration(trans_sp).style("opacity",0);
+})
+
+
+// DAVINCI
+
+dav.on("click", function(){
+	if (dav_state == 1){
+		d3.selectAll(".element").transition().duration(trans_op).attr("opacity",0);
+		d3.selectAll(".op_element").transition().duration(trans_op).attr("opacity",0);
+		d3.selectAll(".op7_element").transition().duration(trans_op).attr("opacity",0);
+		d3.selectAll(".platy").transition().duration(trans_op).attr("opacity",0);
+		circ_dav.transition().duration(trans_exp).attr("r",expand_rad).attr("opacity",1);
+		d3.select(this).transition().attr("opacity",1);
+		speech_dav.transition().duration(trans_sp).style("opacity",0);
+
+		page_dav.style("visibility","visible");
+		page_dav.transition().duration(trans_exp).style("opacity",1);
+
+		dav_state = 0;
+		d3.select(this).moveToFront();
+
+		cad.moveToBack();
+		viz.moveToBack();
+		wra.moveToBack();
+
+	}
+	else {
+		d3.selectAll(".element").transition().delay(trans_exp-2*trans_op).duration(trans_op).attr("opacity",opacity);
+		d3.selectAll(".op_element").transition().delay(trans_op).duration(trans_op).attr("opacity",1);
+		d3.selectAll(".platy").transition().duration(trans_exp-2).attr("opacity",1);
+		d3.selectAll(".op7_element").transition().delay(trans_op).duration(trans_op).attr("opacity",.7);
+		circ_dav.transition().duration(trans_exp).attr("r",rad).attr("opacity",opacity);
+
+		page_dav.transition().duration(trans_exp).style("opacity",0);
+		page_dav.style("visibility","hidden");
+
+		dav_state = 1;
+
+		cad.moveToFront();
+		viz.moveToFront();
+		wra.moveToFront();
+	
+	}
+});
+
+dav.on("mouseover", function(){
+	if (dav_state == 1){
+	speech_dav.transition().duration(trans_sp).style("opacity",0.7);}
+})
+dav.on("mouseout", function(){
+	speech_dav.transition().duration(trans_sp).style("opacity",0);
+})
+
+
+// VIZARD
 
 viz.on("click", function(){
 	if (viz_state == 1){
@@ -190,6 +425,14 @@ viz.on("click", function(){
 
 		viz_state = 0;
 		d3.select(this).moveToFront();
+
+		dav.moveToBack();
+		cad.moveToBack();
+		wra.moveToBack();
+		
+		setTimeout(function(){ 
+			twi_viz.style("display","block")}, 1000);
+
 	}
 	else {
 		d3.selectAll(".element").transition().delay(trans_exp-2*trans_op).duration(trans_op).attr("opacity",opacity);
@@ -202,7 +445,13 @@ viz.on("click", function(){
 		page_viz.style("visibility","hidden");
 
 		viz_state = 1;
-	
+		
+		dav.moveToFront();
+		cad.moveToFront();
+		wra.moveToFront();
+
+		twi_viz.style("display","none");
+
 	}
 });
 
